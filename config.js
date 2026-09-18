c4226da101c1be9bf9af5d0440e489760a495040
Mwcoggs <michaelcoggeshall7@gmail.com>
2026-09-17 21:55:32 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.HUB_CONFIG = { SUPABASE_URL: "PASTE_PROJECT_URL", SUPABASE_KEY: "PASTE_PUBLISHABLE_KEY" };
+window.HUB_CONFIG = { SUPABASE_URL: "https://phcvntrihnatevqvgmyb.supabase.co", SUPABASE_KEY: "sb_publishable_f4OREY2-gJDxemXkZN45dA_pR2_QXmB" };
